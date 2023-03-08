78192ba48eadd4ec3914e4b60d5f60c6bc09997f
Jo-Tanatsit <jotanatsit@gmail.com>
2023-03-08 20:54:21 +0700

    feat(footer): add footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { Flex, Image, Text } from '@chakra-ui/react'
+import "../componentsStyle/Footer.css"
+
+export function Footer() {
+   return (
+      <Flex bg="#F1F1F1" h="18.89vh" w="100%" justify="space-around" align="center" mt="8.89vh">
+         <Flex w="255px" justify="space-between">
+            <Image src="../src/icons/footer-section/squircle.svg" alt="mail" boxSize="2.67vh" />
+            <Text className='email common-font-style'>tanatsit1500@gmail.com</Text>
+         </Flex>
+         <Text className='copy-right common-font-style'>© Copyright 2023 | tanatsit1500</Text>
+         <Flex w="4.167vw" justify="space-between" position="relative" left="-5.56vw">
+            <Image src="../src/icons/footer-section/instagram.svg" alt="instagram" boxSize="2.67vh" />
+            <Image src="../src/icons/footer-section/telegram.svg" alt="mail" boxSize="2.67vh" />
+         </Flex>
+      </Flex>
+   )
+}
